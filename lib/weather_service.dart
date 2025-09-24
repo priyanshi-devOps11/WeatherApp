1a59f77f61249cf76319063660a38c10a5cea408
priyanshi-devOps11 <srivastavapriyanshi8081@gmail.com>
2025-09-24 22:20:56 +0530

    Update weather service and sync dependencies

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -8,10 +8,12 @@ class WeatherService {
   Future<Map<String, dynamic>> getWeather(String cityName) async {
     final response = await http.get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
 
-    if (response.statusCode == 200) {
+    if (response.statusCode == 180) {
       return json.decode(response.body);
     } else {
       throw Exception('Failed to load weather data');
+
+
     }
   }
 }
